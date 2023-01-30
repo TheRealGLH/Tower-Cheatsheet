727be7cdd6e754aedf5dcc90fc0e487d20f60bbb
TheRealGLH <therealglh@users.noreply.github.com>
2023-01-30 11:26:01 +0100

    Adds incredibly rudimentary support for enemy damage calculation

--- src/app/components/sheets/ruby-sheet/ruby-sheet.component.ts
+++ src/app/components/sheets/ruby-sheet/ruby-sheet.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {calculate, Generations, Pokemon, Move, Result} from '@smogon/calc';
 
 @Component({
   selector: 'app-ruby-sheet',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./ruby-sheet.component.css']
 })
 export class RubySheetComponent {
+  gen = Generations.get(3); // alternatively: const gen = 5;
+  myPokemon =   new Pokemon(this.gen, 'Gengar', {
+    item: 'Wiseglasses',
+    nature: 'Timid',
+    evs: {spa: 252}
+  });
+  trainerPokemon =   new Pokemon(this.gen, 'Chansey', {
+    item: 'Leftovers',
+    nature: 'Calm',
+    evs: {hp: 252, spd: 252},
+  });
+  result = this.recalculate();
+
+ngOnInit(){
+  console.log(this.result);
+}
+
+selectedTrainerPokemon(pokemon: Pokemon){
+  this.trainerPokemon = pokemon;
+  this.result = this.recalculate();
+}
+
+//Todo: make this go both ways or something. This solution is just rudamentary for now.
+recalculate():Result{
+  return calculate(
+    this.gen,
+    this.myPokemon,
+    this.trainerPokemon,
+    new Move(this.gen, 'Giga Drain')
+  );
+}
+
 
 }
+
+
--- src/app/components/trainer-picker/ruby-trainer-picker/ruby-trainer-picker.component.ts
+++ src/app/components/trainer-picker/ruby-trainer-picker/ruby-trainer-picker.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import {Pokemon} from '@smogon/calc';
 
 @Component({
   selector: 'app-ruby-trainer-picker',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./ruby-trainer-picker.component.css']
 })
 export class RubyTrainerPickerComponent {
+  @Output() selectedPokemonEvent = new EventEmitter<Pokemon>();
+  trainerPokemon:Pokemon[] = [
+    new Pokemon(
+      3,
+      'Blissey',
+      {level:50,
+      item:'Leftovers'}
+    ),
+    new Pokemon(
+      3,
+      'Alakazam',
+      {level:50,
+      item:'Oran Berry'}
+    ),
+    new Pokemon(
+      3,
+      'Shuckle',
+      {level:100,
+      item:'Berry Juice'}
+    ),
+    new Pokemon(
+      3,
+      'Machamp',
+      {level:50,
+      item:'Choice Band'}
+    ),
+  ];
+  selectedPokemon:Pokemon = this.trainerPokemon[0];
 
+  selectPokemon(pokemon:Pokemon):void{
+    this.selectedPokemon = pokemon;
+    this.selectedPokemonEvent.emit(pokemon);
+  }
 }
